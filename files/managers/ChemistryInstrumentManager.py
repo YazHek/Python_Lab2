76a3e0a4e0619f76448e38a2cd5bcd93e2362c12
YazHek <kruchokjarema@gmail.com>
2019-05-30 08:50:29 +0300

    Final code for lab13

--- files/managers/ChemistryInstrumentManager.py
+++ files/managers/ChemistryInstrumentManager.py
@@ -5,18 +5,18 @@ class ChemistryInstrumentManager:
 
     def sort_by_year(self, reverse=True):
         return sorted(self.chemistry_instrument_list,
-         key=lambda chemistry_instrument:
-          chemistry_instrument.year_of_release, reverse=reverse)
+                      key=lambda chemistry_instrument:
+                      chemistry_instrument.year_of_release, reverse=reverse)
 
     def sort_by_type(self, reverse=False):
         return sorted(self.chemistry_instrument_list,
-         key=lambda chemistry_instrument:
-          chemistry_instrument.type_of_instruments.value, reverse=reverse)
+                      key=lambda chemistry_instrument:
+                      chemistry_instrument.type_of_instruments.value, reverse=reverse)
 
     def find_by_type(self, type_of_instruments):
         return list(filter
-        (lambda chemistry_instrument:chemistry_instrument.type_of_instruments==type_of_instruments,
-         self.chemistry_instrument_list))
+                    (lambda chemistry_instrument: chemistry_instrument.type_of_instruments == type_of_instruments,
+                     self.chemistry_instrument_list))
 
     def output(self, list):
         for chemistry_instrument in list:
